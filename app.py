1b30bb366c6a40bd963df0728f057c90747a19cf
mjalbert13 <mjalbert13@gmail.com>
2020-02-01 15:42:11 -0500

    added more logic

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def translate(word):
     w = word.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y  / N " % get_close_matches(w, data.keys())[0])
         if yn == "y":
@@ -20,4 +24,10 @@ def translate(word):
 
 word = input("Enter a word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
