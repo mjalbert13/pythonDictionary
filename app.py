bfd55e66784b3909a15aa66bc4b76361eb900b52
mjalbert13 <mjalbert13@gmail.com>
2020-02-01 15:18:59 -0500

    added user functions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,14 @@ def translate(word):
     w = word.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y  / N " % get_close_matches(w, data.keys())[0])
+        if yn == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "n":
+            return "Word does not exist. Please try again."
+        else:
+            return "Sorry I did not understand that word."
     else:
         return "The word does not exist. Please try again."
 
